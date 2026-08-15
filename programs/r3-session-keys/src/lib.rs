29bd9fc4c42583a45a21549721e8246a796271c6
Gregory The Dev <gregorythedev@pm.me>
2026-08-15 13:09:17 +0200

    init stub

---
+++ programs/r3-session-keys/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("r3xx1495USK8vysHAfL83d9seSofMH77ytxjuhepfFH");
+
+#[program]
+pub mod r3_session_keys {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
